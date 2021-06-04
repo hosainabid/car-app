756f6b9062bc8b635cfc279fa66cd5aa2bd0bd96
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-06-04 12:04:49 +0600

    styled car items

--- components/CarList/index.js
+++ components/CarList/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text, FlatList, Dimensions} from 'react-native';
-import CarItem from "../CarItem";
+// import CarItem from "../CarItem";
 
 import styles from './styles';
 import cars from './cars';
@@ -10,7 +10,7 @@ const CarsList = (props) => {
     <View style={styles.container}>
       <FlatList
         data={cars}
-        renderItem={({item}) => <CarItem car={item} />}
+        
         showsVerticalScrollIndicator={false}
         snapToAlignment={'start'}
         decelerationRate={'fast'}
