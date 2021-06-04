9fd4124b4942b70c68973b9719751e1ea3e41283
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-06-04 12:29:22 +0600

    make car items

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CarItems';
 import CarsList from './components/CarList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is test!</Text>
-      <CarsList></CarsList>
+      <CarItem/>
       <StatusBar style="auto" />
     </View>
   );
